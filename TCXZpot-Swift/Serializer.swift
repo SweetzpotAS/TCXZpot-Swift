36dc22cc2c20bf1f7ffcb8912a94e9fbde744d22
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-05-19 11:08:17 +0200

    Create protocol to serialize TCX elements

---
+++ TCXZpot-Swift/Serializer.swift
@@ -0,0 +1,13 @@
+//
+//  Serializer.swift
+//  TCXZpot-Swift
+//
+//  Created by Tomás Ruiz López on 19/5/17.
+//  Copyright © 2017 SweetZpot. All rights reserved.
+//
+
+import Foundation
+
+protocol Serializer {
+    func print(line : String)
+}
