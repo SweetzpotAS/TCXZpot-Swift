17a35a1b6ff5d89286606b268e5cb82ebc1380e0
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-05-19 11:01:50 +0200

    Initial Commit

---
+++ TCXZpot-Swift/TCXZpot-Swift.h
@@ -0,0 +1,19 @@
+//
+//  TCXZpot-Swift.h
+//  TCXZpot-Swift
+//
+//  Created by Tomás Ruiz López on 19/5/17.
+//  Copyright © 2017 Tomás Ruiz-López. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TCXZpot-Swift.
+FOUNDATION_EXPORT double TCXZpot_SwiftVersionNumber;
+
+//! Project version string for TCXZpot-Swift.
+FOUNDATION_EXPORT const unsigned char TCXZpot_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TCXZpot_Swift/PublicHeader.h>
+
+
