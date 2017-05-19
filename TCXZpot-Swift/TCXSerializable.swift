15be582a4318dd2cd3c384f9b4958c37c72212da
Tomás Ruiz-López <trl.ironside@gmail.com>
2017-05-19 11:33:23 +0200

    Create protocol to serialize to TCX format

---
+++ TCXZpot-Swift/TCXSerializable.swift
@@ -0,0 +1,13 @@
+//
+//  TCXSerializable.swift
+//  TCXZpot-Swift
+//
+//  Created by Tomás Ruiz López on 19/5/17.
+//  Copyright © 2017 SweetZpot. All rights reserved.
+//
+
+import Foundation
+
+protocol TCXSerializable {
+    func serialize(to serializer : Serializer)
+}
